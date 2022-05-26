34dea216eb95e82bb75824dd5a2e6d2034846a31
Sanghun Oh <otter.oh@gmail.com>
2022-05-26 14:24:26 +0900

    build weather project

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/with_vue" : "/",
+  outputDir: "docs",
+};
