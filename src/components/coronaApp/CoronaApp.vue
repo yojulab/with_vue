217c2337fd6f6c2a46a6a1b24c3bfd8b538685e1
Sanghun Oh <otter.oh@gmail.com>
2022-05-27 17:08:30 +0900

    add element-plus

--- src/components/coronaApp/CoronaApp.vue
+++ src/components/coronaApp/CoronaApp.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header>Header</el-header>
+      <el-header><HeaderLayout /></el-header>
       <el-container>
-        <el-aside width="200px">Aside</el-aside>
-        <el-main>Main</el-main>
+        <el-aside width="200px"><AsideLayout /></el-aside>
+        <el-main><MainLayout /></el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 
 <script>
-export default {};
+import HeaderLayout from "./layouts/HeaderLayout.vue";
+import AsideLayout from "./layouts/AsideLayout.vue";
+import MainLayout from "./MainLayout.vue";
+
+export default {
+  components: {
+    HeaderLayout,
+    AsideLayout,
+    MainLayout,
+  },
+};
 </script>
 
 <style></style>
---
+++ src/components/coronaApp/layouts/AsideLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-row class="tac">
+    <el-col>
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span>Navigator One</span>
+          </template>
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item two</el-menu-item>
+        </el-sub-menu>
+        <el-sub-menu index="2">
+          <template #title>
+            <el-icon><setting /></el-icon>
+            <span>Navigator Two</span>
+          </template>
+          <el-menu-item index="2-1">item one</el-menu-item>
+          <el-menu-item index="2-2">item two</el-menu-item>
+        </el-sub-menu>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import { Location, Setting } from "@element-plus/icons-vue";
+export default {
+  components: {
+    Location,
+    Setting,
+  },
+};
+</script>
+
+<style></style>
