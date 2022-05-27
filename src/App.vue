737fd9677f2d069f340162104ff47a9c975255ef
Sanghun Oh <otter.oh@gmail.com>
2022-05-27 16:02:02 +0900

    add CoronaApp

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
-  <WeatherApp></WeatherApp>
+  <keep-alive>
+    <component :is="currentApp"></component>
+  </keep-alive>
 </template>
 
 <script>
-import WeatherApp from "./components/weatherapp/WeatherApp.vue";
+import WeatherApp from "./components/weatherApp/WeatherApp.vue";
+import CoronaApp from "./components/coronaApp/CoronaApp.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      currentApp: "CoronaApp", // 0 = WeatherApp, 1 = CoronaApp,
+    };
+  },
   components: {
     WeatherApp,
+    CoronaApp,
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+
+// css framework
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/docs/css/bootstrap.min.css";
+import "element-plus";
+import "element-plus/dist/index.css";
 
 createApp(App).mount("#app");
